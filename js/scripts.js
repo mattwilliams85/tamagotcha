01105f7f6958b60768e4de9272b9cd259c7ff0b2
Matthew <mattjunk85@yahoo.com>
2014-07-24 18:50:37 -0700

    cleans up animated bars

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 // PROTOTYPE //
 var Tamagotcha = {
-  // Checks if Tamagotcha is still alive //
-  statusCheck: function() {
-    if (this.foodLevel === 0) {
-      return false;
-      console.log("YOU KILLED HIM")
-    } else {
-      return true;
-    }
-  },
   // Initializes starting stats for Tamagotcha //
   initialize: function(initializedName) {
     this.name = initializedName;
@@ -22,26 +13,29 @@ var Tamagotcha = {
 
 
 var countDown = function(myPet) {
-  myPet.foodLevel -= 5;
-  myPet.sleepLevel -= 5;
-  myPet.moodLevel -= 2.5;
+  myPet.foodLevel -= 1;
+  myPet.sleepLevel -= 1;
+  myPet.moodLevel -= 0.5;
   $('#foodNum').text(myPet.foodLevel);
   $('#sleepNum').text(myPet.sleepLevel);
   $('#moodNum').text(myPet.moodLevel);
 // Stat bar animations //
   $('#foodBar').animate({
-  height: myPet.foodLevel
+  marginTop: 101 - myPet.foodLevel,
+  height: myPet.foodLevel,
   });
   $('#sleepBar').animate({
+  marginTop: 101 - myPet.sleepLevel,
   height: myPet.sleepLevel
   });
   $('#moodBar').animate({
+  marginTop: 101 - myPet.moodLevel,
   height: myPet.moodLevel
   });
 // Death check //
 
 }
-
+// var barHeight = percent * $element.width() / 100;
 $(document).ready(function(){
 
 // Names Pet and starts Game //
@@ -50,21 +44,21 @@ $(document).ready(function(){
     $('.hideMe').show();
     var myPet = Object.create(Tamagotcha);
     myPet.initialize($('#input').val());
-    countDown(myPet);
-    var time = setInterval(function() {
+
     $('span').text(myPet.name.toUpperCase());
-
-
+// Timer Set //
+    var time = setInterval(function() {
+      countDown(myPet);
 // Death conditon //
-    // if (myPet.foodLevel <= 0 || myPet.sleepLevel <= 0 || myPet.moodLevel <= 0) {
-    //   clearInterval(time);
-    // }
-    if (myPet.foodLevel <= 0) {
-      console.log("RIP :'(")
-    }
+      if (myPet.foodLevel <= 0 || myPet.sleepLevel <= 0 || myPet.moodLevel <= 0) {
+        clearInterval(time);
+      }
+      if (myPet.foodLevel <= 0) {
+        console.log("RIP :'(")
+      }
     },1000);
-// Feeds Tamagotcha //
-    $('#feed').click(function(){
+// Button animation and cooldown //
+    $('.button').click(function(){
       event.preventDefault();
       $(".button").attr("disabled", "disabled");
       $('.button').fadeTo(500,0.5)
@@ -72,13 +66,26 @@ $(document).ready(function(){
         $('.button').fadeTo(250,1)
         $(".button").removeAttr("disabled");
       }, 3000);
-      if (myPet.foodLevel < 90) {
-        myPet.foodLevel += 5;
+    });
+// Feed button //
+    $('#feed').click(function(){
+     if (myPet.foodLevel < 90) {
+        myPet.foodLevel += 10;
         $('#foodBar').css("height",myPet.foodLevel)
       } else {
-        $('#message').text("He's not hungry right now...");
-      }
-
+        $('#message').text("He's not hungry right now...").fadeOut(2000);
+        myPet.moodLevel -= 10;
+      };
+    });
+// Sleep button //
+  $('#sleep').click(function(){
+       if (myPet.sleepLevel < 90) {
+        myPet.sleepLevel += 10;
+        $('#sleepBar').css("height",myPet.sleepLevel)
+      } else {
+        $('#message').text("He's not sleepy right now...").fadeOut(2000);
+        myPet.moodLevel -= 10;
+      };
     });
   });
 });
